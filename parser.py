c0e24d31b9ef6722c982d35535d4f7f913d17e31
Graf2242 <graf2242@gmail.com>
2019-03-17 14:34:31 +0300

    Repo init

---
+++ parser.py
@@ -0,0 +1,16 @@
+import re
+
+
+def read_file(file_path):
+    with open(file_path, 'r')as f:
+        return f.read()
+
+
+def filter_lines(regexp, lines):
+    return [line for line in lines if re.match(regexp, line)]
+
+
+filename = "coverage-error.log"
+regexp = r"\[\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:d\d\].+"
+lines = read_file(filename)
+print(filter_lines(regexp, lines))
