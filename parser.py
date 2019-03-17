4f6762de014848226f9bab9504ec381cca66a25b
Graf2242 <graf2242@gmail.com>
2019-03-17 14:45:08 +0300

    printing don't match strings

--- parser.py
+++ parser.py
@@ -7,6 +7,12 @@ def read_file(file_path):
 
 
 def filter_lines(regexp, lines):
+    result = []
+    for line in lines:
+        if re.match(regexp, line):
+            result.append(line)
+        else:
+            print(f"line {line} don't match")
     return [line for line in lines if re.match(regexp, line)]
 
 
