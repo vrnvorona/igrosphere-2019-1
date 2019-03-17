8b0274bbb78244312e0c1f2c1a9129a366bb8332
Graf2242 <graf2242@gmail.com>
2019-03-17 14:39:30 +0300

    add if __main__

--- parser.py
+++ parser.py
@@ -10,7 +10,8 @@ def filter_lines(regexp, lines):
     return [line for line in lines if re.match(regexp, line)]
 
 
-filename = "coverage-error.log"
-regexp = r"\[\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:\d\d\].+"
-lines = read_file(filename)
-print(len(filter_lines(regexp, lines)))
+if __name__ == "__main__":
+    filename = "coverage-error.log"
+    regexp = r"\[\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:\d\d\].+"
+    lines = read_file(filename)
+    print(filter_lines(regexp, lines))
